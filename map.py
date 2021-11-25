872bdde4e649dee093666bf380093dbb490c4032
Louis-David Perron <100434291+perronld@users.noreply.github.com>
2021-11-25 15:39:30 -0500

    CLIM-18 renamed new routes/functions

--- map.py
+++ map.py
@@ -37,11 +37,11 @@ def get_choro_values(partition, var, model, month='ann'):
                     mimetype='application/json')
 
 
-def get_grid_hover_values(lat, lon, var, model, month):
+def get_delta_30y_gridded_values(lat, lon, var, model, month):
     """
     Fetch specific data within the delta30y dataset
-    ex:  curl 'http://localhost:5000/get-grid-hover-values/60.31062731740045/-100.06347656250001/tx_max/rcp85/ann?period=1951&decimals=2'
-        curl 'http://localhost:5000/get-grid-hover-values/60.31062731740045/-100.06347656250001/tx_max/rcp85/ann?period=1951&decimals=2&delta7100=true'
+    ex:  curl 'http://localhost:5000/get-delta-30y-gridded-values/60.31062731740045/-100.06347656250001/tx_max/rcp85/ann?period=1951&decimals=2'
+        curl 'http://localhost:5000/get-delta-30y-gridded-values/60.31062731740045/-100.06347656250001/tx_max/rcp85/ann?period=1951&decimals=2&delta7100=true'
     :param lat: latitude (float as string)
     :param lon: longitude (float as string)
     :param var: climate variable name
@@ -78,11 +78,11 @@ def get_grid_hover_values(lat, lon, var, model, month):
     return {p: round(delta_30y_slice[f"{model}_{var}{delta}_{p}"].item(), decimals) for p in ['p10', 'p50', 'p90']}
 
 
-def get_regional_hover_values(partition, index, var, model, month):
+def get_delta_30y_regional_values(partition, index, var, model, month):
     """
     Fetch specific data within the delta30y partitionned dataset (health/census/...)
-        ex: curl 'http://localhost:5000/get-regional-hover-values/census/4510/tx_max/rcp85/ann?period=1951'
-            curl 'http://localhost:5000/get-regional-hover-values/census/4510/tx_max/rcp85/ann?period=1951&delta7100=true'
+        ex: curl 'http://localhost:5000/get-delta-30y-regional-values/census/4510/tx_max/rcp85/ann?period=1951'
+            curl 'http://localhost:5000/get-delta-30y-regional-values/census/4510/tx_max/rcp85/ann?period=1951&delta7100=true'
     :param partition: partition name
     :param index: ID of the region (int as string)
     :param var: climate variable name
