d05003fdc952a9013dbdb56f70425fc7f361fdca
francisPLT <francis.pelletier@crim.ca>
2022-10-17 14:36:49 -0400

    Add tests

---
+++ tests/api/test_api_charts.py
@@ -0,0 +1,40 @@
+import pytest
+import requests
+
+
+@pytest.mark.parametrize(
+    "url,status_code",
+    [
+        pytest.param(
+            "http://localhost:5000/generate-charts/60.31062731740045/-100.06347656250001/tx_max/ann",
+            200,
+            id="generate_charts",
+        ),
+        pytest.param(
+            "http://localhost:5000/generate-charts/60.31062731740045/-100.06347656250001/tx_max/ann?dataset_name=CMIP6",
+            200,
+            id="generate_charts_cmip6",
+        ),
+        pytest.param(
+            "http://localhost:5000/generate-charts/60.31062731740045/-100.06347656250001/spei_12m/ann",
+            200,
+            id="generate_spei_charts",
+        ),
+        pytest.param(
+            "http://localhost:5000/generate-charts/58.031372421776396/-61.12792968750001/slr/ann",
+            200,
+            id="generate_slr_charts",
+        ),
+        pytest.param(
+            "http://localhost:5000/generate-regional-charts/census/4510/tx_max/ann", 200, id="generate_regional_charts"
+        ),
+        pytest.param(
+            "http://localhost:5000/generate-regional-charts/census/4510/tx_max/ann?dataset_name=CMIP6",
+            200,
+            id="generate_regional_charts_cmip6",
+        ),
+    ],
+)
+def test_api_charts(url: str, status_code: int):
+    response = requests.get(url)
+    assert response.status_code == status_code
---
+++ tests/api/test_api_map.py
@@ -0,0 +1,55 @@
+import pytest
+import requests
+
+
+@pytest.mark.parametrize(
+    "url,status_code",
+    [
+        pytest.param(
+            "http://localhost:5000/get-choro-values/census/tx_max/rcp85/ann/?period=1971", 200, id="get_choro_values"
+        ),
+        pytest.param(
+            "http://localhost:5000/get-choro-values/census/tx_max/ssp585/ann/?period=2071&dataset_name=CMIP6&delta7100=true",
+            200,
+            id="get_choro_values_cmip6",
+        ),
+        pytest.param(
+            "http://localhost:5000/get-delta-30y-gridded-values/60.31062731740045/-100.06347656250001/tx_max/ann?period=1951&decimals=2",
+            200,
+            id="get_delta_30y_gridded_values",
+        ),
+        pytest.param(
+            "http://localhost:5000/get-delta-30y-gridded-values/60.31062731740045/-100.06347656250001/tx_max/ann?period=1951&decimals=2&delta7100=true",
+            200,
+            id="get_delta_30y_gridded_values_delta7100",
+        ),
+        pytest.param(
+            "http://localhost:5000/get-delta-30y-gridded-values/60.31062731740045/-100.06347656250001/tx_max/ann?period=1951&decimals=2&dataset_name=CMIP6",
+            200,
+            id="get_delta_30y_gridded_values_cmip6",
+        ),
+        pytest.param(
+            "http://localhost:5000/get-slr-gridded-values/61.04/-61.11?period=2050",
+            200,
+            id="get_slr_gridded_values"
+        ),
+        pytest.param(
+            "http://localhost:5000/get-delta-30y-regional-values/census/4510/tx_max/ann?period=1951",
+            200,
+            id="get_delta_30y_regional_values",
+        ),
+        pytest.param(
+            "http://localhost:5000/get-delta-30y-regional-values/census/4510/tx_max/ann?period=1951&delta7100=true",
+            200,
+            id="get_delta_30y_regional_values_delta7100",
+        ),
+        pytest.param(
+            "http://localhost:5000/get-delta-30y-regional-values/census/4510/tx_max/ann?period=1951&dataset_name=CMIP6",
+            200,
+            id="get_delta_30y_regional_values_cmip6",
+        ),
+    ],
+)
+def test_api_map(url: str, status_code: int):
+    response = requests.get(url)
+    assert response.status_code == status_code
