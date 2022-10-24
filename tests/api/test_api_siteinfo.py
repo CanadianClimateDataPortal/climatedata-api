3a2fb5083cf88e1466be62502eb74ed8a92585e6
francisPLT <francis.pelletier@crim.ca>
2022-10-24 14:45:13 -0400

    Generalize test_api_siteinfo test

--- tests/api/test_api_siteinfo.py
+++ tests/api/test_api_siteinfo.py
@@ -6,33 +6,26 @@ import requests
 from tests.configs import TEST_HOST_URL
 
 
+def _check_json_validity(response_content):
+    try:
+        json.loads(response_content)
+    except ValueError:
+        return False
+    return True
+
+
 @pytest.mark.parametrize(
-    "url,status_code,expected",
+    "url,status_code",
     [
         pytest.param(
             f"{TEST_HOST_URL}/get-location-values/45.551538/-73.744616?dataset_name=cmip5",
             200,
-            {
-                "tg_mean": {
-                    "1951": "6.3",
-                    "1971": "6.6",
-                    "2021": "8.9",
-                    "2051": "10.9",
-                    "2071": "12.5"
-                },
-                "prcptot": {
-                    "1951": "966.6",
-                    "1971": "1002.4",
-                    "delta_2021_percent": "8",
-                    "delta_2051_percent": "12",
-                    "delta_2071_percent": "15"}
-            },
             id="get_location_values"
         )
     ]
 )
-def test_api_siteinfo_json_loads(url: str, status_code: int, expected: dict):
+def test_api_siteinfo_json_loads(url: str, status_code: int):
     response = requests.get(url)
-    content = json.loads(response.content)
+    is_json_valid = _check_json_validity(response.content)
     assert response.status_code == status_code
-    assert content == expected
+    assert is_json_valid
